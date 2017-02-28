62bc0a66099472a7b40fbfce58228f2f8ae5fa2a
Chris Harrell <chris.harrell@sq1.com>
2017-02-27 22:23:29 -0600

    Actually add assets, along with 3 basic scense

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	private int min;
+	private int max;
+	public int guess;
+	public string guessText;
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void QuitGame() {
+		Debug.Log ("Quit game requested");
+		Application.Quit();
+	}
+}
