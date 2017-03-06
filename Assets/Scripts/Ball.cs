97b6fa87a91efc0c6bd64201e4fdc8b0f590d9d9
Chris Harrell <chris.harrell@sq1.com>
2017-03-05 20:11:22 -0600

    Project the ball outward on mouse click

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,14 +5,26 @@ public class Ball : MonoBehaviour {
 
 	public Paddle paddle;
 	private Vector3 paddleToBallVector;
+	private bool ballInPlay;
 	
 	// Use this for initialization
 	void Start () {
+		ballInPlay = false;
 		paddleToBallVector = this.transform.position - paddle.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = paddle.transform.position + paddleToBallVector;
+	
+		if(!ballInPlay) {
+			// Hold the ball on the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// If mouse button is clicked, start the game
+			if(Input.GetMouseButtonDown(0)) {
+				ballInPlay = true;
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}
 	}
 }
