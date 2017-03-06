61e98702fdabf7a9b71ed10d1b5e2eba019abca7
Chris Harrell <chris.harrell@sq1.com>
2017-03-05 19:14:45 -0600

    Add mouse control for paddle

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	Vector3 paddlePos;
+	float mousePosInBlocks;
+	
+	// Update is called once per frame
+	void Update () {		
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		
+		paddlePos = new Vector3(Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f), this.transform.position.y, this.transform.position.z);
+		this.transform.position = paddlePos;
+	}
+}
