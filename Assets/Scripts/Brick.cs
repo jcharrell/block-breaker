946d5ed9a0114b911b85d9e870fbb00fc9f60c9e
Chris Harrell <chris.harrell@sq1.com>
2017-03-11 15:15:16 -0600

    Consolidate max hits tracking and add invincible brick

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,31 +3,42 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+	public Sprite[] hitSprites;
+	
+	private int maxHits;
 	private int timesHit;
 	private LevelManager levelManager;
-	public Sprite[] hitSprites;
 	
 	// Use this for initialization
 	void Start () {
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 		timesHit = 0;
+		maxHits = hitSprites.Length + 1;
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision) {
+		if(this.tag == "breakable") {
+			HandleHits ();
+		}
+	}
+	
+	void HandleHits() {
 		++this.timesHit;
 		
-		if(this.timesHit >= this.maxHits) {
+		if(this.timesHit >= maxHits) {
 			Destroy(gameObject);
 		} else {
 			LoadSprites();
 		}
-		
 	}
 	
 	void LoadSprites() {
 		int spriteIndex = timesHit - 1;
-		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		
+		// If a sprite is not defined at the index, do not attempt to use it
+		if(hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
 	}
 	
 	// Update is called once per frame
